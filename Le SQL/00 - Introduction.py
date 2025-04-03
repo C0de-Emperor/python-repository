dda740dd2fc6092d07354071398eaf00215260f0
Emperor <mttmallet@gmail.com>
2025-04-03 19:58:06 +0200

    sql introduction

---
+++ Le SQL/00 - Introduction.py
@@ -0,0 +1,25 @@
+"""
+Le SQL est un langage qui sert au stockage, a l'organisation et a la manipulation de données.
+
+
+Pour commencer, nous avons besoin d'un logiciel qui va stocker nos données. 
+Le plus simple et visuel que je connaisse est SQLiteStudio, facilement téléchargeable.
+"""
+
+"""
+Pour créer une nouvelle base de données pressez "Ctrl + o"
+
+Paramètres :
+    - "Type de base de données" : SQLite3
+    - "Fichier" : 
+        > "Create a new database file"
+
+!!! Retenez bien l'endroit ou vous allez enregistrer votre base de donnée !!!
+"""
+
+"""
+Normalement votre base de données doit apparaitre à gauche.
+
+Pour pouvoir interagir avec elle, vous devez vous y "connecter" (petit symbole de prise branchée).
+Et naturellement pour arreter d'interagir avec la base il faut se deconnecter.
+"""
